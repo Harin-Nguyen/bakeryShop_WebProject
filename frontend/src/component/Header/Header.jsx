30e6e3a93205a39038021841ca835eb9a962d675
huyennguyen230103@gmail.com <huyennguyen230103@gmail.com>
2024-11-18 09:19:53 +0700

    navBar-headers-menuList

---
+++ frontend/src/component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite cakes here!</h2>
+            <p>Flourish & Frost offers delightful baked goods made with the finest ingredients. From fresh bread to exquisite pastries and cakes, we have something for every occasion. Order today and experience the magic!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
