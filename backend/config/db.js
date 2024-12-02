817d5d189ce24d8a10d5178395765c48d0c37011
kha <hoangkhatruong2003@gmail.com>
2024-12-02 17:32:36 +0700

    setup backend

---
+++ backend/config/db.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.MONGODB_URI) {
+  console.error("MongoDB URI is not defined in the environment variables.");
+  process.exit(1);
+} else {
+  console.log("MongoDB URI:", process.env.MONGODB_URI);
+}
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
