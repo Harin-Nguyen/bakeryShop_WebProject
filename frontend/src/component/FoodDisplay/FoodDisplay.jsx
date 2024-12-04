d3b5fcb1cb6bee8fabce78b0605c27304d9d0704
huyennguyen230103@gmail.com <huyennguyen230103@gmail.com>
2024-12-04 21:43:55 +0700

    footer

--- frontend/src/component/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/component/FoodDisplay/FoodDisplay.jsx
@@ -12,7 +12,9 @@ const FoodDisplay = ({category}) => {
       <h2>Top cakies near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index)=>{
-            return <FoodItem key={item} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+            if (category===item.category || category==="All"){
+              return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+            } 
         })}
       </div>
     </div>
