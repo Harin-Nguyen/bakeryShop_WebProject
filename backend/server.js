7f5aee5c44d7b039447e19d6f0dd88be88dfdb57
21126073 <pngkhiem21@vp.fitus.edu.vn>
2024-12-13 20:54:27 +0700

    backed: User routes
    Signed-off-by: 21126073 <pngkhiem21@vp.fitus.edu.vn>

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import foodRouter from "./routes/foodRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
 import cartRouter from "./routes/cartRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 import cors from "cors";
 import  connectDB from "./config/db.js";
 
@@ -31,3 +32,4 @@ app.use("/api/food",foodRouter);
 app.use("/images",express.static('uploads'));
 app.use("/api/cart",cartRouter);
 app.use("/api/order",orderRouter);
+app.use("/api/user",userRouter);
