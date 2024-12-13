b0cc153a7bc83b75b46b6023f2a8752349f2adb4
21126073 <pngkhiem21@vp.fitus.edu.vn>
2024-12-13 21:27:23 +0700

    web: Full login-logout-register function * Worked normally, needs an UI fix xD Signed-off-by: 21126073 <pngkhiem21@vp.fitus.edu.vn>

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,6 +5,9 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
+    const url = "http://localhost:5000";
+    const [token, setToken] = useState("");
+    const [food_list, setFoodList] = useState([]);
 
     const addToCart = (itemId) => {
         if  (!cartItems[itemId]){
@@ -42,7 +45,10 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
     return(
         <StoreContext.Provider value={contextValue}>
