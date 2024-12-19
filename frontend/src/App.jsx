c998551e314c47a3105979b7bf2f5d546b743a77
huyennguyen230103@gmail.com <huyennguyen230103@gmail.com>
2024-12-19 10:35:16 +0700

    user orders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './component/Footer/Footer'
 import LoginPopup from './component/LoginPopup/LoginPopup'
+import MyOrders from './pages/MyOrders/MyOrders'
 
 const App = () => {
 
@@ -20,6 +21,7 @@ const App = () => {
           <Route path='/' element={<Home/>} />
           <Route path='/cart' element={<Cart/>} />
           <Route path='/order' element={<PlaceOrder/>} />
+          <Route path='/myorders' element={<MyOrders/>}  />
         </Routes>
       </div>
       <Footer />
