e336561e79b734025bc5d754136d1b0e05b662a5
huyennguyen230103@gmail.com <huyennguyen230103@gmail.com>
2024-12-19 16:56:53 +0700

    fix

---
+++ frontend/src/pages/Product/Product.jsx
@@ -0,0 +1,14 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext';
+
+const Product = () => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Product
